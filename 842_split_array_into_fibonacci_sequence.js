2995ce579467de1c3ba6a72c74e47937f97d9603
zhangky <iszhangky@163.com>
2020-12-09 21:14:45 +0800

    提交842,将数组拆分成斐波那契序列:补充实现

--- 842_split_array_into_fibonacci_sequence.js
+++ 842_split_array_into_fibonacci_sequence.js
@@ -41,6 +41,42 @@ const backtrack = (list, S, length, index, sum, prev) => {
     return false;
 }
 
+var splitIntoFibonacci = function(S) {
+    let result = [];
+    /**
+     * 
+     * 终止条件：如果数组长度大于2
+     * 当前分割数据不能已0开头，如果以0开头则break;
+     * 如果当前num > pre1 + pre2 则结束这次循环。因为之后的数据都是大于
+     * 如果小于，则下次循环，如果=则满足push
+     */
+    backTrack(0);
+    return result;
+
+    function backTrack(sIndex) {
+        if (result.length >= 3 && sIndex === S.length) {
+            return true;
+        }
+
+        let curStr = '';
+        for (let i = sIndex; i < S.length; i++) {
+            curStr += S[i];
+            if (i > sIndex && curStr.startsWith('0')) break;
+            let curNum = Number(curStr);
+            if (curNum > Math.pow(2, 31) - 1) break;
+            if (result.length >= 2 && curNum > (result[result.length - 1] + result[result.length - 2])) break;
+            if (result.length < 2 || curNum === (result[result.length - 1] + result[result.length - 2])) {
+                result.push(curNum);
+                if (backTrack(i + 1)) {
+                    return true;
+                } 
+                result.pop();
+            }
+        }
+        return false;
+    }
+};
+
 // const S = "123456579";      //[123,456,579]
 // const S = "11235813";   //[1,1,2,3,5,8,13]
 // const S = "112358130";      //[]
